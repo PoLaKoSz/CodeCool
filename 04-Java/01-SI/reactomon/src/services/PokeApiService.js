e58d905113d985946f840de75a9563e0f163e1b9
Tom PoLáKoSz <polakosz@freemail.hu>
2020-06-14 21:37:29 +0200

    Replace fetch API with axio npm package

--- 04-Java/01-SI/reactomon/src/services/PokeApiService.js
+++ 04-Java/01-SI/reactomon/src/services/PokeApiService.js
@@ -1,19 +1,19 @@
+import axios from "axios";
+
 export default class PokeApiService {
   constructor() {
     this.baseURL = "https://pokeapi.co/api/v2/";
   }
 
   getAll() {
-    return fetch(this.baseURL + "pokemon?limit=100")
-      .then((response) => response.json())
-      .then((json) =>
-        json.results.map((pokemon) => {
-          pokemon.id = pokemon.url
-            .substring(`${this.baseURL}pokemon/`.length)
-            .slice(0, -1);
-          pokemon.sprite = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${pokemon.id}.png`;
-          return pokemon;
-        })
-      );
+    return axios.get(this.baseURL + "pokemon?limit=100").then((json) =>
+      json.data.results.map((pokemon) => {
+        pokemon.id = pokemon.url
+          .substring(`${this.baseURL}pokemon/`.length)
+          .slice(0, -1);
+        pokemon.sprite = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${pokemon.id}.png`;
+        return pokemon;
+      })
+    );
   }
 }
