2466c4f6c9e6938003c3750075d006a54c298bd1
Tom PoLáKoSz <polakosz@freemail.hu>
2020-06-15 09:32:04 +0200

    [WIP] Working on the Header component

--- 04-Java/01-SI/reactomon/src/components/Header.js
+++ 04-Java/01-SI/reactomon/src/components/Header.js
@@ -1,10 +1,21 @@
 import React from "react";
+import logo from "./../img/logo.jpg";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 function Header() {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <Container>
+      <img src={logo} className="img-fluid" alt="logo" />
+      <Row>
+        <Col className="text-center">
+          <Button className="mr-1">Pokémons</Button>
+          <Button className="ml-1">Types</Button>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
